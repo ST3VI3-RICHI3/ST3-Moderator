1bd72017c784d3c209c9e965cc6b499d3ee2d1d0
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2020-01-07 03:38:58 +0000

    Added role managment.
    Added a role command to better manage roles in a server.

---
+++ Commands/Role.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+class Role(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def role(self, ctx, func = None, User: discord.Member = None, role: discord.role = None):
+		if ctx.message.author.guild_permissions.administrator:
+		if User != None:
+			if Role != None:
+    			if func == "add":
+					User.add_roles(role)
+				elif func == "remove":
+					User.remove_roles(role)
+				else:
+					ctx.send("Missing or invalid arguement, `func` (Function). Usage: " + str(self.bot.command_prefix) + "role {Add / Remove} @User @Role_Name")
+			else:
+				ctx.send("Missing or invalid arguement, `role` (@Role_To_Act_On). Usage: " + str(self.bot.command_prefix) + "role {Add / Remove} @User @Role_Name")
+		else:
+			ctx.send("Missing or invalid arguement, `User` (@User_Mention). Usage: " + str(self.bot.command_prefix) + "role {Add / Remove} @User @Role_Name")
+
+def setup(bot):
+    print("[Info] Role commands successfully loaded.")
+    bot.add_cog(Role(bot))
