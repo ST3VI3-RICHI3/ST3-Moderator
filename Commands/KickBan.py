dea6bcfdc18236afba868ebdfe1a3a7ab7abf72a
ST3VI3-RICHI3 <bourbon9.as@gmail.com>
2020-02-18 16:36:55 +0000

    Merged kick and ban commands

--- Commands/KickBan.py
+++ Commands/KickBan.py
@@ -2,10 +2,25 @@ import discord
 import asyncio
 from discord.ext import commands
 
-class Ban(commands.Cog):
+class KickBan(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def kick(self, ctx, user: discord.Member = None, *, Reason: str = "No reason provided"):
+        if ctx.message.author.guild_permissions.administrator:
+            if user == None:
+                msg = await ctx.send(":x: Missing arg: `User`")
+                await asyncio.sleep(5)
+                await msg.delete()
+                await ctx.message.delete()
+            else:
+                msg = await ctx.send(":ballot_box_with_check:<@{}> was kicked for: {}".format(user.id, Reason))
+                await user.kick(reason=Reason)
+                await asyncio.sleep(5)
+                await msg.delete()
+                await ctx.message.delete()
 
     @commands.command()
     async def ban(self, ctx, user: discord.Member = None, *, Reason: str = "No reason provided"):
@@ -23,4 +38,4 @@ class Ban(commands.Cog):
                 await ctx.message.delete()
 
 def setup(bot):
-    bot.add_cog(Ban(bot))
+    bot.add_cog(KickBan(bot))
