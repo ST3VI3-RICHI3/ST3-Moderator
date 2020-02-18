094805c212e03cae8efa9b34ae82ced4820c7665
ST3VI3-RICHI3 <bourbon9.as@gmail.com>
2020-02-18 16:28:20 +0000

    Updated mute to include LTAP and remute

--- Commands/Mute.py
+++ Commands/Mute.py
@@ -9,13 +9,102 @@ class Mute(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        if str(member.id) in str(Shared.Vars.DBData):
+            if str(member.guild.id) in str(Shared.Vars.DBData[str(member.id)]):
+                if "Muted" in str(Shared.Vars.DBData[str(member.id)][str(member.guild.id)]):
+                    if Shared.Vars.DBData[str(member.id)][str(member.guild.id)][0]["Muted"]:
+                        Guilds = Shared.Database.Load("Guilds.json")
+                        if str(member.guild.id) in Guilds:
+                            if Guilds[str(member.guild.id)]["Mute_Ban_On_Leave"]:
+                                embed=discord.Embed(title="Ban", description=f"Ban from: {member.guild.name}", color=0xff0000)
+                                embed.add_field(name="Reason", value=f"You have been banned from {member.guild.name} for L.T.A.P (Leaving to avoid punishment). This ban is: perminant.", inline=False)
+                                try:
+                                    await member.send(embed=embed)
+                                except:
+                                    pass
+                                Reason = "LTAP, left the server while muted."
+                                await member.ban(reason=Reason)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        MRole: discord.role = None
+        Guilds = Shared.Database.Load("Guilds.json")
+        if str(member.guild.id) in Guilds:
+            if Guilds[str(member.guild.id)]["Mute_Role"] != None:
+                MRole = Guilds[str(member.guild.id)]["Mute_Role"]
+                MRole = member.guild.get_role(MRole)
+            else:
+                for r in member.guild.roles:
+                    if str(r).lower() == "muted":
+                        MRole = member.guild.get_role(r.id)
+                if MRole == None:
+                    return
+        else:
+            for r in member.guild.roles:
+                if str(r).lower() == "muted":
+                    MRole = member.guild.get_role(r.id)
+            if MRole == None:
+                return
+        if str(member.id) in str(Shared.Vars.DBData):
+            if str(member.guild.id) in str(Shared.Vars.DBData[str(member.id)]):
+                if "Muted" in str(Shared.Vars.DBData[str(member.id)][str(member.guild.id)]):
+                    if Shared.Vars.DBData[str(member.id)][str(member.guild.id)][0]["Muted"]:
+                        for r in member.roles:
+                            if "@everyone" not in str(r):
+                                await member.remove_roles(r)
+                        await member.add_roles(MRole)
+
+    @commands.command()
+    async def confmute(self, ctx, option = None, value = None):
+        if ctx.message.author.guild_permissions.administrator:
+            if option == None or value == None:
+                if option == None:
+                    msg = await ctx.send("Missing required argument `option`.")
+                    await asyncio.sleep(5)
+                    await ctx.message.delete()
+                    await msg.delete()
+                elif value == None:
+                    msg = await ctx.send("Missing required argument `value`.")
+                    await asyncio.sleep(5)
+                    await ctx.message.delete()
+                    await msg.delete()
+            else:
+                Guilds = Shared.Database.Load("Guilds.json")
+                if str(ctx.message.guild.id) not in Guilds:
+                    Guilds[str(ctx.message.guild.id)] = {}
+                    Guilds[str(ctx.message.guild.id)]["Mute_Role"] = None
+                    Guilds[str(ctx.message.guild.id)]["Mute_Ban_On_Leave"] = False
+                if option.lower() == "ltap":
+                    if value.lower() == "true":
+                        Guilds[str(ctx.message.guild.id)]["Mute_Ban_On_Leave"] = True
+                    if value.lower() == "false":
+                        Guilds[str(ctx.message.guild.id)]["Mute_Ban_On_Leave"] = False
+                Shared.Database.dump("Guilds.json", Guilds)
+                msg = await ctx.send(f"Updated `{option}` to `{value}`.")
+                await asyncio.sleep(5)
+                await ctx.message.delete()
+                await msg.delete()
+
     @commands.command()
     async def mute(self, ctx, user: discord.Member = None):
         MRole: discord.role = None
         Guilds = Shared.Database.Load("Guilds.json")
         if str(ctx.message.guild.id) in Guilds:
-            MRole = Guilds[str(ctx.message.guild.id)]["Mute_Role"]
-            MRole = ctx.message.guild.get_role(MRole)
+            if Guilds[str(ctx.message.guild.id)]["Mute_Role"] != None:
+                MRole = Guilds[str(ctx.message.guild.id)]["Mute_Role"]
+                MRole = ctx.message.guild.get_role(MRole)
+            else:
+                for r in ctx.message.guild.roles:
+                    if str(r).lower() == "muted":
+                        MRole = ctx.message.guild.get_role(r.id)
+                if MRole == None:
+                    msg = await ctx.send(f":x: I cannot find a role named `muted` for this guild. If your role is under a different name, turn on anyone can @ mention this role, then run `{Shared.Vars.prefix}setmute @mtue_role`. Anyone can @ mention this role can then be turned off if need be.")
+                    await asyncio.sleep(30)
+                    await ctx.message.delete()
+                    await msg.delete()
+                    return
         else:
             for r in ctx.message.guild.roles:
                 if str(r).lower() == "muted":
@@ -114,8 +203,19 @@ class Mute(commands.Cog):
             MRole: discord.role = None
             Guilds = Shared.Database.Load("Guilds.json")
             if str(ctx.message.guild.id) in Guilds:
-                MRole = Guilds[str(ctx.message.guild.id)]["Mute_Role"]
-                MRole = ctx.message.guild.get_role(MRole)
+                if Guilds[str(ctx.message.guild.id)]["Mute_Role"] != None:
+                    MRole = Guilds[str(ctx.message.guild.id)]["Mute_Role"]
+                    MRole = ctx.message.guild.get_role(MRole)
+                else:
+                    for r in ctx.message.guild.roles:
+                        if str(r).lower() == "muted":
+                            MRole = ctx.message.guild.get_role(r.id)
+                    if MRole == None:
+                        msg = await ctx.send(f":x: I cannot find a role named `muted` for this guild. If your role is under a different name, turn on anyone can @ mention this role, then run `{Shared.Vars.prefix}setmute @mtue_role`. Anyone can @ mention this role can then be turned off if need be.")
+                        await asyncio.sleep(30)
+                        await ctx.message.delete()
+                        await msg.delete()
+                        return
             else:
                 for r in ctx.message.guild.roles:
                     if str(r).lower() == "muted":
