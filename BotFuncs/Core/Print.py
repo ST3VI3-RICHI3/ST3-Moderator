608bda2e7b285fb82dee7c31042ef68948f29343
ST3VI3 RICHI3 <bourbon9.as@gmail.com>
2020-11-28 16:02:51 +0000

    Update print to allow for other types.

--- BotFuncs/Core/Print.py
+++ BotFuncs/Core/Print.py
@@ -4,4 +4,6 @@ def prt(msg:str, type="info", end="\n", log=True):
     ostr = msg
     if type.lower() == "info":
         ostr = f"[i] {msg}"
+    else:
+        ostr = f"[{type}] {msg}"
     print(ostr, end=end)
