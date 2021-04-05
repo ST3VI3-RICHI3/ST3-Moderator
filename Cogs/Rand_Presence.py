388ada695f9f308d1ff5a4088e72142647446446
ST3VI3-RICHI3 <bourbon9.as@gmail.com>
2021-04-05 14:48:18 +0100

    Update copyright dates

--- Cogs/Rand_Presence.py
+++ Cogs/Rand_Presence.py
@@ -1,6 +1,6 @@
 """
     ST3-Moderator, a moderation bot for discord
-    Copyright (C) 2020  ST3VI3 RICHI3
+    Copyright (C) 2021  ST3VI3 RICHI3
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
